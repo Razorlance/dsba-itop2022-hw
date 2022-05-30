bc51c0b7a611c6d5d9f97f59f1816c203c3ff316
Denis Kulakov <8269905@gmail.com>
2022-05-30 17:07:32 +0300

    Added mainwindow markup

--- nba_game/mainwindow.cpp
+++ nba_game/mainwindow.cpp
@@ -1,15 +1,19 @@
 #include "mainwindow.h"
+
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
+MainWindow::~MainWindow() { delete ui; }
 
+void MainWindow::on_add_button_clicked() {}
+
+void MainWindow::on_team_button_clicked() {}
+
+void MainWindow::on_delete_button_clicked() {}
+
+void MainWindow::on_search_button_clicked() {}
--- nba_game/mainwindow.h
+++ nba_game/mainwindow.h
@@ -4,18 +4,24 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui
+{
+class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-public:
+   public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private:
+   private slots:
+    void on_add_button_clicked();
+
+   private:
     Ui::MainWindow *ui;
 };
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
